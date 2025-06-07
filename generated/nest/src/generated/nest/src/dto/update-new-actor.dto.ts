e396ecbc236015c61d56b6fa6aba0155d57826db
netor98 <pcu18021379@gmail.com>
2025-06-06 21:16:16 -0700

    Merge into main

--- generated/nest/src/generated/nest/src/dto/update-new-actor.dto.ts
+++
@@ -1,5 +0,0 @@
-// generated/nest/src/dto/update-new-actor.dto.ts
-import { PartialType } from '@nestjs/swagger';
-import { CreateNewActorDto } from './create-new-actor.dto';
-
-export class UpdateNewActorDto extends PartialType(CreateNewActorDto) {}
